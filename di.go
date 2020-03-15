e00d212e96017803b33fe050599411a8be168d10
Behruz Valiev <behruzvaliev@gmail.com>
2020-03-16 01:05:59 +0500

    ref: di

--- di.go
+++ di.go
@@ -6,6 +6,21 @@ import (
 	"reflect"
 )
 
+// ErrTargetNil ...
+var ErrTargetNil = errors.New("errors target cannot be nil")
+
+// ErrNoSuchComponent ...
+var ErrNoSuchComponent = errors.New("no such components")
+
+// ErrTargetNilPtr ...
+var ErrTargetNilPtr = errors.New("target must be a non-nil pointer")
+
+// ErrAssignComponentPtr ...
+var ErrAssignComponentPtr = errors.New("can't assign component to pointer")
+
+// ErrComponentUnmetDependencies ...
+var ErrComponentUnmetDependencies = errors.New("some components has unmet dependencies")
+
 type definitions struct {
 	dependencies int
 	constructor  reflect.Value
@@ -13,9 +28,9 @@ type definitions struct {
 
 // Container ...
 type Container struct {
-	// Хранить созданные компоненты
+	// Contains created components
 	components map[reflect.Type]interface{}
-	// Хранить определения на базе которых создаются компоненты
+	// Contains definitions are components based
 	definitions map[reflect.Type]definitions
 }
 
@@ -27,10 +42,37 @@ func NewContainer() *Container {
 	}
 }
 
-// Provide - регистрация компонентов + их связывание (wire)
+// Provide - registered components + wired they
 func (c *Container) Provide(constructors ...interface{}) {
 	c.register(constructors)
 	c.wire()
+}
+
+// Component ...
+func (c *Container) Component(target interface{}) {
+	if target == nil {
+		panic(ErrTargetNil)
+	}
+
+	targetValue := reflect.ValueOf(target)
+	targetType := targetValue.Type()
+	targetValueType := targetValue.Elem().Type()
+
+	value, ok := c.components[targetValueType]
+	if !ok {
+		panic(ErrNoSuchComponent)
+	}
+
+	if targetType.Kind() != reflect.Ptr || targetValue.IsNil() {
+		panic(ErrTargetNilPtr)
+	}
+
+	targetElemType := targetType.Elem()
+	if !reflect.TypeOf(value).AssignableTo(targetElemType) {
+		panic(ErrAssignComponentPtr)
+	}
+
+	targetValue.Elem().Set(reflect.ValueOf(value))
 }
 
 // register of dependencies
@@ -88,35 +130,7 @@ func (c *Container) wire() {
 		}
 
 		if wired == 0 {
-			// TODO: return component list (rest)
-			panic(errors.New("some components has unmet dependencies"))
+			panic(ErrComponentUnmetDependencies)
 		}
 	}
-}
-
-// Component ...
-func (c *Container) Component(target interface{}) {
-	if target == nil {
-		panic("errors target cannot be nil")
-	}
-
-	targetValue := reflect.ValueOf(target)       // get value
-	targetType := targetValue.Type()             // get value type
-	targetValueType := targetValue.Elem().Type() // get value type type
-
-	value, ok := c.components[targetValueType]
-	if !ok {
-		panic(errors.New("no such components"))
-	}
-
-	if targetType.Kind() != reflect.Ptr || targetValue.IsNil() {
-		panic(errors.New("target must be a non-nil pointer"))
-	}
-
-	targetElemType := targetType.Elem()
-	if !reflect.TypeOf(value).AssignableTo(targetElemType) {
-		panic(errors.New("can't assign component to pointer"))
-	}
-
-	targetValue.Elem().Set(reflect.ValueOf(value))
 }
